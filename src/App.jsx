8d450612407d0a243976a4db2d333cf96c0a75ab
ania221B <100519699+ania221B@users.noreply.github.com>
2023-10-19 22:17:09 +0200

    elements added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import data from './data'
 import Accordion from './Accordion'
 import { useState } from 'react'
+import Noise from './Noise'
+import Circle from './Circle'
 
 function App () {
   const [questions, setQuestions] = useState(data)
   return (
     <main>
       <Accordion questions={questions}></Accordion>
+      <Noise></Noise>
+      <Circle className='circle-top'></Circle>
+      <Circle className='circle-bottom'></Circle>
     </main>
   )
 }
